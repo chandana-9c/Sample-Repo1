ace801fecae8315b299e1b2b598fac9a2eae362e
Ubuntu <ubuntu@ip-172-31-65-153.ec2.internal>
2026-01-29 06:37:57 +0000

    commit to repo - 2

--- jenkins-demo-1/File1.py
+++ jenkins-demo-1/File1.py
@@ -3,3 +3,5 @@ echo "print('Hello Jenkins!!!)" > File1.py
 print('My new pipeline')
 print('Hi')
 print('Good morningcd ..')
+print('Good morninggit push -u origin main..')
+print('...............')
